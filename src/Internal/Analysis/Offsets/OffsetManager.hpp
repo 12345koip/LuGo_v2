bdd808a3509c5cc8f0067a5b1f3ba23bb0c8c5f4
12345koip <piok8393@gmail.com>
2025-12-16 10:04:37 +0000

    Added basic offsetmanager skeleton might change over time

---
+++ src/Internal/Analysis/Offsets/OffsetManager.hpp
@@ -0,0 +1,58 @@
+/*
+This file was created by
+12345koip for the LuGo executor
+on 15/12/2025. See LICENSE and README
+for details.
+*/
+
+#pragma once
+
+#include <unordered_map>
+#include "Common/Attributes.hpp"
+#include "PointerObfuscation.hpp"
+
+namespace LuGo::Analysis::Offsets {
+
+    enum class RawPointerOffsetRef: uint8_t {
+        luaC_step,
+        luaC_fullgc,
+        luaH_new,
+
+        RBX_Instance_PushInstance,
+        RBX_ScriptContext_ResumeImpl
+    };
+
+    enum class ObfuscatedPointerRef: uint8_t {
+        ScriptContext, //for ScriptContext::resumeImpl.
+    };
+
+    class OffsetManager final {
+        private:
+            std::unordered_map<RawPointerOffsetRef, uintptr_t> rawPointerOffsets {};
+            std::unordered_map<ObfuscatedPointerRef, std::shared_ptr<ObfuscatedPointer>> obfuscatedPointers {};
+
+            OffsetManager() = default;
+
+        public:
+            static OffsetManager& GetSingleton() {
+                static OffsetManager singleton;
+                return singleton;
+            }
+
+            L_NODISCARD L_FORCEINLINE std::shared_ptr<ObfuscatedPointer> GetPointerObfuscation(const ObfuscatedPointerRef ref) const {
+                return (this->obfuscatedPointers.contains(ref)? this->obfuscatedPointers.at(ref): nullptr);
+            }
+
+            void SetPointerObfuscation(const ObfuscatedPointerRef ref, const std::shared_ptr<ObfuscatedPointer> offset) {
+                this->obfuscatedPointers.emplace(ref, offset);
+            }
+
+            L_NODISCARD L_FORCEINLINE uintptr_t GetPointerOffset(const RawPointerOffsetRef ref) const {
+                return (this->rawPointerOffsets.contains(ref)? this->rawPointerOffsets.at(ref): 0x00);
+            }
+
+            L_FORCEINLINE void SetPointerOffset(const RawPointerOffsetRef ref, const uintptr_t offset) {
+                this->rawPointerOffsets.emplace(ref, offset);
+            }
+    };
+}
